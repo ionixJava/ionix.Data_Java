420e5c2bdd1433479b72c7dd4240395be3aa1041
Mehmet Goren <mehmetgoren@outlook.com>
2016-02-14 16:52:06 +0200

    1

--- src/ionix/Data/EntityCommandSelect.java
+++ src/ionix/Data/EntityCommandSelect.java
@@ -6,6 +6,8 @@ import ionix.Utils.Ref;
 import java.lang.reflect.Field;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EntityCommandSelect {
 
@@ -96,13 +98,33 @@ public class EntityCommandSelect {
         return entity;
     }
 
-    public <TEntity> TEntity querySingle(Class<TEntity> cls, EntityMetaDataProvider provider, SqlQuery query){
-        if (null == query)
-            throw new IllegalArgumentException ("query is null");
 
-        EntityMetaData metaData = SqlQueryHelper.ensureCreateEntityMetaData(cls, provider);
+    private <TEntity> List<TEntity> readEntityList(EntityMetaData metaData, SqlQuery query, MapType mapType)
+    {
+        ArrayList<TEntity> ret = new ArrayList<>();
 
-        return this.readEntity(metaData, query, MapType.Query);
+        QueryResult qr = null;
+        try
+        {
+            qr = this.getDataAccess().executeQuery(query);
+            ResultSet rs = qr.getResultSet();
+
+            try {
+                while (rs.next()){
+                    TEntity entity = (TEntity) metaData.getEntityClass().newInstance();
+                    this.map(entity, metaData, qr, mapType);
+                    ret.add(entity);
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        finally
+        {
+            if (qr != null) qr.close();
+        }
+
+        return ret;
     }
 
     public <TEntity> TEntity selectSingle(Class<TEntity> cls, EntityMetaDataProvider provider, SqlQuery extendedQuery)
@@ -116,4 +138,35 @@ public class EntityCommandSelect {
 
         return this.readEntity(metaData, query, MapType.Select);
     }
+
+    public <TEntity> List<TEntity> select(Class<TEntity> cls, EntityMetaDataProvider provider, SqlQuery extendedQuery){
+        EntityMetaData metaData = SqlQueryHelper.ensureCreateEntityMetaData(cls, provider);
+
+        SqlQueryProviderSelect builder = new SqlQueryProviderSelect(metaData);
+        SqlQuery query = builder.toQuery();
+        if (null != extendedQuery)
+            query.combine(extendedQuery);
+
+        return this.readEntityList(metaData, query, MapType.Select);
+    }
+
+    public <TEntity> TEntity querySingle(Class<TEntity> cls, EntityMetaDataProvider provider, SqlQuery query){
+        if (null == query)
+            throw new IllegalArgumentException ("query is null");
+
+        EntityMetaData metaData = SqlQueryHelper.ensureCreateEntityMetaData(cls, provider);
+
+        return this.readEntity(metaData, query, MapType.Query);
+    }
+
+    public <TEntity> List<TEntity> query(Class<TEntity> cls, EntityMetaDataProvider provider, SqlQuery query){
+        if (null == query)
+            throw new IllegalArgumentException ("query is null");
+
+        EntityMetaData metaData = SqlQueryHelper.ensureCreateEntityMetaData(cls, provider);
+
+        return this.readEntityList(metaData, query, MapType.Query);
+    }
+
+    selectById de kaldık. Bunun İçin filtercriteria ve sql helper metodları gerekli
 }
